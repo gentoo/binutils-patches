7f9c134f5f06de3090412800f100b971b7027409
Sam James <sam@gentoo.org>
2026-06-09 00:35:15 +0100

    make-tarball.sh: update for kup and other changes in glibc-patches.git
    Sync with:
    
    commit b0a763af81de96f14da47af020eb8326d5b6d2a1 (HEAD -> master, origin/master, origin/HEAD)
    Author:     Andreas K. Hüttel <dilfridge@gentoo.org>
    AuthorDate: Wed May 6 15:49:50 2026 +0200
    Commit:     Andreas K. Hüttel <dilfridge@gentoo.org>
    CommitDate: Wed May 6 15:55:58 2026 +0200
    
        Automate tagging and upload more
    
        Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>
    
    in glibc-patches.git.
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- make-tarball.sh
+++ make-tarball.sh
@@ -3,26 +3,31 @@
 mypathbinutils=${GENTOO_BINUTILS_REPO:-~/Gentoo/misc/binutils-gdb}
 mypathpatches=${GENTOO_BINUTILS_PATCHES_REPO:-~/Gentoo/misc/binutils-patches}
 
+# I plan to extend this script a bit, to also help with the initial
+# Gentoo release branch creation. That's why I check so much for both
+# git repos... -A
+
 PN="binutils"
-PV=$1
+PV=${1%/}
 pver=$2
 
 if [[ -z ${PV} ]] ; then
 	echo "Usage: $0 binutils-version patchset-version-to-be-created"
+	echo "Important environment variables: GENTOO_BINUTILS_REPO GENTOO_BINUTILS_PATCHES_REPO"
 	echo "Please read the script before trying to use it :)"
 	exit 1
 fi
 
 # check that we have a gentoo binutils patches git repo
 
-if [[ ! -f "${mypathpatches}/README.Gentoo.patches" ]] || [[ ! -d "${mypathpatches}/All" ]] ; then
+if [[ ! -f "${mypathpatches}/README.Gentoo.patches" ]] || [[ ! -d "${mypathpatches}/.git" ]] ; then
 	echo "Error: GENTOO_BINUTILS_PATCHES_REPO needs to point to the main directory of a Gentoo binutils patchset git clone"
 	exit 1
 fi
 
 # check that we have a gentoo binutils git repo
 
-if [[ ! -f "${mypathbinutils}/COPYING.LIBGLOSS" ]] || [[ ! -d "${mypathbinutils}/.git" ]] ; then
+if [[ ! -d "${mypathbinutils}/bfd" ]] || [[ ! -d "${mypathbinutils}/.git" ]] ; then
 	echo "Error: GENTOO_BINUTILS_REPO needs to point to the main directory of a Gentoo binutils-gdb git clone"
 	exit 1
 fi
@@ -41,7 +46,7 @@ fi
 
 mydescpatches=$(git describe)
 
-# go into the binutils glibc repo
+# go into the gentoo binutils repo
 
 cd "${mypathbinutils}"
 
@@ -53,7 +58,7 @@ cd "${mypathbinutils}"
 mybranchinfo=$(git status --porcelain -b|grep '^##')
 mybranch=$(echo ${mybranchinfo}|sed -e 's:^## ::' -e 's:\.\.\..*$::')
 if [[ ! "gentoo/binutils-${PV%_p?}" == "${mybranch}" ]] ; then
-	echo "Error: Your git repository is on the incorrect branch ${mybranch}; should be gentoo/binutils-${PV%_p?}"
+	echo "Error: Your binutils git repository is on the incorrect branch ${mybranch}; should be gentoo/binutils-${PV%_p?}"
 	exit 1
 fi
 
@@ -61,9 +66,11 @@ fi
 
 mystatusinfo=$(git status --porcelain)
 if [[ ! -z "${mystatusinfo}" ]] ; then
-	echo "Error: Your working directory is not clean"
+	echo "Error: Your binutils working directory is not clean"
 	exit 1
 fi
+
+mydescbinutils=$(git describe)
 
 # check if the tag already exists
 
@@ -80,34 +87,17 @@ if [[ -e tmp ]] || [[ -e ${PN}-${PV}-patches-${pver}.tar.xz ]] ; then
 	exit 1
 fi
 rm -rf tmp
-rm -f ${PN}-${PV}-*.tar.bz2
+rm -f ${PN}-${PV}-*.tar.xz
 
 for myname in 0*.patch ; do
-	if [[ -e "${myname}" ]]; then
+	if [[ -e ${myname} ]]; then
 		echo "Error: ${myname} exists in git"
 		exit 1
 	fi
 done
 rm -f 0*.patch
 
-# check if we have to override the upstream tag
-
-if [[ "${PV}" == "9999" ]]; then
-	# working with master is not supported anymore
-	echo "Patchsets for git master are not supported anymore"
-    exit 1
-fi
-
-mytaginfo=$(git tag -l|grep "gentoo/binutils-${PV}-upstream")
-if [[ ! -z "${mytaginfo}" ]] ; then
-	starttag="gentoo/binutils-${PV}-upstream"
-else
-	starttag="binutils-${PV//./_}"
-fi
-
-echo "Starting from tag ${starttag}"
-
-mkdir -p tmp/patch
+mkdir -p tmp/patches
 
 # copy README.Gentoo.patches
 
@@ -118,7 +108,23 @@ echo "Generated with make-tarball.sh ${mydescpatches}" >> "tmp/README.Gentoo.pat
 
 # create and rename patches
 
-git format-patch --no-base --no-cover-letter ${starttag}..HEAD > /dev/null || exit 1
+if [[ "${PV}" == "9999" ]]; then
+	# working with master is not supported anymore
+	echo "Patchsets for git master are not supported anymore"
+	exit 1
+else
+	mytaginfo=$(git tag -l|grep "gentoo/binutils-${PV}-upstream")
+	if [[ ! -z "${mytaginfo}" ]] ; then
+		startpoint="gentoo/binutils-${PV}-upstream"
+	else
+		# release branch, start from upstream release tag
+		startpoint="binutils-${PV//./_}"
+	fi
+
+	echo "Starting from tag ${starttag}"
+fi
+
+git format-patch --no-base --no-cover-letter ${startpoint}..HEAD > /dev/null || exit 1
 
 # remove all patches where the summary line starts with:
 # - [no-tarball]: not related to upstream tarball
@@ -129,26 +135,39 @@ rm -f 0???-no-tarball-*.patch
 rm -f 0???-no-patch-*.patch
 rm -f 0???-Automatic-date-update-in-version.in.patch
 
-for myname in 0*.patch ; do
-	mv ${myname} tmp/patch/
-done
-
 # add the extra patch
 
-cp "${mypathpatches}/All/9999-Gentoo-We-make-a-release" tmp/patch/9999-Gentoo-We-make-a-release.patch || exit 1
+cp "${mypathpatches}/All/9999-Gentoo-We-make-a-release" tmp/patches/9999-Gentoo-We-make-a-release.patch || exit 1
+
+# move patches into temporary directory
+
+mv 0*.patch tmp/patches/ || exit 1
 
 # add a history file
 
-git log --stat --decorate ${starttag}..HEAD > tmp/patch/README.history || exit 1
+git log --stat --decorate ${startpoint}..HEAD > tmp/patches/README.history || exit 1
 
 # package everything up
 
+echo "Packaging up ${PN}-${PV}-patches-${pver}.tar.xz"
 tar -Jcf ${PN}-${PV}-patches-${pver}.tar.xz \
-	-C tmp patch README.Gentoo.patches || exit 1
+	-C tmp patches README.Gentoo.patches || exit 1
 rm -r tmp
 
 du -b *.tar.xz
 
+# sign the patchset with gpg
+
+echo "Signing ${PN}-${PV}-patches-${pver}.tar.xz with gpg"
+gpg -b -o ${PN}-${PV}-patches-${pver}.tar.xz.sig ${PN}-${PV}-patches-${pver}.tar.xz || exit 1
+
+echo "Uploading file to /pub/proj/toolchain/binutils/patches/"
+kup putraw ${PN}-${PV}-patches-${pver}.tar.xz ${PN}-${PV}-patches-${pver}.tar.xz.sig /pub/proj/toolchain/binutils/patches/ || exit 1
+
 # tag the commit
 
-git tag -s -m "Gentoo patchset binutils-${PV}-${pver}" "gentoo/binutils-${PV}-${pver}"
+echo "Tagging the patchset commit gentoo/binutils-${PV}-${pver}"
+git tag -s -m "Gentoo patchset binutils-${PV}-${pver}" "gentoo/binutils-${PV}-${pver}" || exit 1
+
+echo "Pushing the tag"
+git push gentoo gentoo/binutils-${PV}-${pver}
